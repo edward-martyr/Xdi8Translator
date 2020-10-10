a490179d45f7a8dd7ccbf95b3de6c2d38e1b816d
edward-martyr <edwardmartyr@outlook.com>
2020-10-10 10:12:06 +0800

    0.0.7

--- Xdi8Translator/translator.py
+++ Xdi8Translator/translator.py
@@ -7,22 +7,23 @@ class Translator():
             ltext=lcut(text)
             text=' '.join(ltext)
             for fro, to in hanzi2xdi8_dict.items():
-                if ord(to[0])>48:
+                if to[0] not in '''!"#$%&'()*+,-./:;<=>?@[\]^_`{¦}~､''':
                     text=text.replace(fro,to)
                 else:
                     text=text.replace(fro,'\b'+to)
         else:
             for fro, to in hanzi2xdi8_dict.items():
-                if ord(to[0])>48:
+                if to[0] not in '''!"#$%&'()*+,-./:;<=>?@[\]^_`{¦}~､''':
                     text=text.replace(fro,' '+to)
                 else:
                     text=text.replace(fro,to)
             text=text[1:]
         text=text.replace('\n ','\n')
-        text=text.replace('( ',' (')
+        for punct in ['(','[','"',"'",'{']:
+            text=text.replace(punct+' ',' '+punct)
         return text
     
     def tidai2xdi8(self, text):
         for fro, to in tidai2xdi8_dict.items():
-                text=text.replace(fro,to)
+            text=text.replace(fro,to)
         return text
