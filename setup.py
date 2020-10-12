5b3f5317ef69178f54f1d147a50b7f7fd3fa74e6
edward-martyr <edwardmartyr@outlook.com>
2020-10-12 19:18:52 +0800

    0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Xdi8Translator',
-    version='0.1.3',
+    version='0.1.4',
     description='Multiway Xdi8 Translator',
     long_description=long_description,
     long_description_content_type="text/markdown",
